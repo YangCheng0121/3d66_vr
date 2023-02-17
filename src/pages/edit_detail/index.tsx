129724c8f6b6fb3403af830b08e81485aa6db87c
YangCheng <yang759126596@163.com>
2023-02-17 19:07:04 +0800

    add

--- src/pages/edit_detail/index.tsx
+++
@@ -1,9 +0,0 @@
-function Edit_detail() {
-  return (
-    <>
-      <h1>我详情页</h1>
-    </>
-  )
-}
-
-export default Edit_detail
---
+++ src/pages/vr/edit_detail/[id]/index.tsx
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <>
+      <h1>我是VR详情</h1>
+    </>
+  )
+}
+
+export default Index
