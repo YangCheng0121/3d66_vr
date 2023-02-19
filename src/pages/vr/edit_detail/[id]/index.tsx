386a2d7c246ce6051cb5176ab7bbf982fc13c3ef
Alfred <yang759126596@163.com>
2023-02-19 23:01:26 +0800

    add

--- src/pages/vr/edit_detail/[id]/index.tsx
+++ src/pages/vr/edit_detail/[id]/index.tsx
@@ -1,9 +1,19 @@
+import styles from './edit_detail.module.scss'
+
+const RightBar = () => {
+  return (
+    <ul className={styles.right_bar}>
+      <li className={styles.right_bar_item}>
+        <span className={styles.right_bar_label}>热点</span>
+      </li>
+    </ul>
+  )
+}
+
 function Index() {
   return (
     <>
-      <ul>
-        <li></li>
-      </ul>
+      <RightBar/>
       <h1>我是VR详情</h1>
     </>
   )
