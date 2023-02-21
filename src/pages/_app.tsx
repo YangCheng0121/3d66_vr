668c8ea24126fd44dd88e7c0949bfffc76bde8a3
Alfred <yang759126596@163.com>
2023-02-21 21:36:40 +0800

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import '@/styles/globals.scss'
 import '@/styles/normalize.scss'
 import type { AppProps } from 'next/app'
+import { ConfigProvider } from 'antd';
+
+type ThemeData = {
+  borderRadius: number;
+  colorPrimary: string;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const defaultData: ThemeData = {
+    borderRadius: 6,
+    colorPrimary: '#19b955',
+  };
+
+  return(
+    <ConfigProvider
+      theme={{ token: { colorPrimary: defaultData.colorPrimary, borderRadius: defaultData.borderRadius } }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
--- src/pages/vr/edit_detail/[id]/index.tsx
+++ src/pages/vr/edit_detail/[id]/index.tsx
@@ -1,18 +1,25 @@
 import styles from './edit_detail.module.scss'
 import dynamic from "next/dynamic";
+import {Button} from "antd";
+import {useState} from "react";
 
 const Krpano = dynamic(
   () => {
     return import('./components/Krpano');
   },
-  { ssr: false },
+  {ssr: false},
 );
 
 const RightBar = () => {
+  const [viewBox, setViewBox] = useState<boolean>(false)
+
   return (
     <ul className={styles.right_bar}>
-      <li className={styles.right_bar_item}>
+      <li className={styles.right_bar_item} onClick={() => setViewBox((value) => {
+        return !value
+      })}>
         <span className={styles.right_bar_label}>初始视角</span>
+        {viewBox && <SetViewBox/>}
       </li>
       <li className={styles.right_bar_item}>
         <span className={styles.right_bar_label}>热点</span>
@@ -21,12 +28,27 @@ const RightBar = () => {
   )
 }
 
+const SetViewBox = () => {
+  return (
+    <div className={styles.set_view_box}>
+      <div className={styles.line_t}></div>
+      <div className={styles.line_r}></div>
+      <div className={styles.line_b}></div>
+      <div className={styles.line_l}></div>
+      <div className={styles.box_lt}></div>
+      <div className={styles.box_rt}></div>
+      <div className={styles.box_rb}></div>
+      <div className={styles.box_lb}></div>
+      <Button type="primary" className={styles.set} onClick={e => e.stopPropagation()}>设为初始视角</Button>
+    </div>
+  )
+}
+
 function Index() {
   return (
     <>
       <Krpano/>
       <RightBar/>
-      <h1>我是VR详情</h1>
     </>
   )
 }
