5ec0258ecc5437e848d56f5b58e80a9066d626e8
Alfred <yang759126596@163.com>
2023-02-24 01:21:38 +0800

    add

--- src/pages/vr/edit_detail/[id]/components/HotSpotList.tsx
+++ src/pages/vr/edit_detail/[id]/components/HotSpotList.tsx
@@ -1,14 +1,60 @@
 import React, {useMemo, useState} from "react";
-import {Row, Space, Typography, Col, Drawer, Button, ConfigProvider} from "antd";
-import {CloseOutlined} from "@ant-design/icons";
+import {Row, Space, Typography, Col, Drawer, Button, ConfigProvider, Dropdown} from "antd";
+import {CloseOutlined, RightOutlined} from "@ant-design/icons";
 import useTriggerDom from "@/hooks/triggerDom";
+import styles from '../edit_detail.module.scss'
 
-const COLOR = '#1D2129'
+const COLOR_PRIMARY_TEXT = '#1D2129'
+const COLOR_Typography_TEXT = '#86909C'
 
 const HotSpotList = (props: any) => {
   const {trigger} = props
   const [open, setOpen] = useState(false)
   const triggerDom = useTriggerDom({open, setOpen, trigger})
+
+  const AllDrown = () => {
+    const [dropDownOpen, setDropDownOpen] = useState(false)
+    const [rotate, setRotate] = useState(0)
+
+    return (
+      <Dropdown
+        trigger={['hover']}
+        onOpenChange={(open) => {
+          setRotate(open ? 90 : 0)
+        }}
+        open={dropDownOpen}
+        dropdownRender={() => {
+          return (
+            <div className={styles.drown}>
+              {
+                [1, 2, 3].map((item: any) => {
+                  return (
+                    <div className={styles.drown_item}
+                         key={item}
+                         onClick={() => setDropDownOpen(false)}
+                    >
+                      <a>
+                        全部热点
+                      </a>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )
+        }}
+      >
+        <a>
+          <Typography.Text style={{color: COLOR_Typography_TEXT}}>
+            全部热点
+            <RightOutlined
+              style={{color: COLOR_Typography_TEXT, transition: 'all .3s', transform: `rotate(${rotate}deg)`}}
+            />
+          </Typography.Text>
+        </a>
+      </Dropdown>
+    )
+  }
 
   return (
     <>
@@ -50,11 +96,28 @@ const HotSpotList = (props: any) => {
             <Typography.Link underline>
               开通全景贵族
             </Typography.Link>
-            享无限容量
+            <Typography style={{color: COLOR_Typography_TEXT}}>
+              享无限容量
+            </Typography>
           </Space.Compact>
-          <Button block type="primary" style={{color: COLOR}}>
+          <div style={{height: '16px'}}></div>
+          <Button block type="primary" style={{color: COLOR_PRIMARY_TEXT}}>
             添加热点
           </Button>
+
+          <div style={{height: '20px'}}></div>
+
+          <Row align="middle">
+            <Col flex={1}>
+              <Typography.Title level={5} style={{color: '#fff'}}>
+                当前热点场景热点(1)
+              </Typography.Title>
+            </Col>
+            <Col flex={1} style={{textAlign: 'right'}}>
+              <AllDrown/>
+            </Col>
+          </Row>
+
         </ConfigProvider>
       </Drawer>
     </>
