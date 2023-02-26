010b17e1d6f94e501b22f4aeff9efe32978458a0
Alfred <yang759126596@163.com>
2023-02-26 19:34:08 +0800

    Update AddSpot.tsx

--- src/pages/vr/edit_detail/[id]/components/AddSpot.tsx
+++ src/pages/vr/edit_detail/[id]/components/AddSpot.tsx
@@ -1,4 +1,4 @@
-import {Drawer, Row, Col, Typography, Collapse, Radio, Space} from "antd";
+import {Drawer, Row, Col, Typography, Collapse, Radio, Space, Form, Input} from "antd";
 import React, {useState} from "react";
 import {CloseOutlined} from "@ant-design/icons";
 import Center from "@/common/Center";
@@ -9,6 +9,7 @@ const AddSpot = (props: any) => {
   const {children} = props
   const [open, setOpen] = useState(false)
   const [type, setType] = useState(0)
+  const [form, setForm] = useState<any>({})
 
   const text = `
   A dog is a type of domesticated animal.
@@ -56,53 +57,58 @@ const AddSpot = (props: any) => {
           </Row>
         }
       >
-        <Collapse
-          bordered={false}
-          defaultActiveKey={['1']}
-          expandIcon={({isActive}) =>
-            <CaretDownFilled style={{color: isActive ? '#F8BC25' : '#C9CDD4'}}
-                             rotate={isActive ? 0 : -90}/>}
-        >
-          <Collapse.Panel
-            style={{border: 'none'}}
-            header={
-              <Typography.Title style={{color: '#fff'}} level={5}>类型</Typography.Title>
-            }
-            key="1">
-            <Radio.Group
-              onChange={(e) => setType(e.target.value)}
-              defaultValue="a"
-              buttonStyle="solid"
-              size="small">
-              <Space>
-                {
-                  [
-                    {label: '场景切换', value: 1},
-                    {label: '超链接', value: 2},
-                    {label: '图片', value: 3},
-                    {label: '文字', value: 4},
-                    {label: '视频', value: 5}
-                  ].map((item) => {
-                    return (
-                      <Radio.Button
-                        className={type !== item.value ? styles.type : styles.type_active}
-                        value={item.value}
-                        key={item.value}>
-                        {item.label}
-                      </Radio.Button>
-                    )
-                  })
-                }
-              </Space>
-            </Radio.Group>
-          </Collapse.Panel>
-          <Collapse.Panel style={{border: 'none'}} header="This is panel header 2" key="2">
-            <Input placeholder="Basic usage" />;
-          </Collapse.Panel>
-          <Collapse.Panel style={{border: 'none'}} header="This is panel header 3" key="3">
-            <p>{text}</p>
-          </Collapse.Panel>
-        </Collapse>
+        <Form>
+          <Collapse
+            bordered={false}
+            defaultActiveKey={['1']}
+            expandIcon={({isActive}) =>
+              <CaretDownFilled style={{color: isActive ? '#F8BC25' : '#C9CDD4'}}
+                               rotate={isActive ? 0 : -90}/>}
+          >
+            <Collapse.Panel
+              style={{border: 'none'}}
+              header={
+                <Typography.Title style={{color: '#fff'}} level={5}>类型</Typography.Title>
+              }
+              key="1">
+              <Radio.Group
+                onChange={(e) => setType(e.target.value)}
+                defaultValue="a"
+                buttonStyle="solid"
+                size="small">
+                <Space>
+                  {
+                    [
+                      {label: '场景切换', value: 1},
+                      {label: '超链接', value: 2},
+                      {label: '图片', value: 3},
+                      {label: '文字', value: 4},
+                      {label: '视频', value: 5}
+                    ].map((item) => {
+                      return (
+                        <Radio.Button
+                          className={type !== item.value ? styles.type : styles.type_active}
+                          value={item.value}
+                          key={item.value}>
+                          {item.label}
+                        </Radio.Button>
+                      )
+                    })
+                  }
+                </Space>
+              </Radio.Group>
+            </Collapse.Panel>
+            <Collapse.Panel style={{border: 'none'}} header="This is panel header 2" key="2">
+              <Form.Item>
+                <Input name="spot_type" placeholder="Basic usage"/>;
+              </Form.Item>
+            </Collapse.Panel>
+            <Collapse.Panel style={{border: 'none'}} header="This is panel header 3" key="3">
+              <p>{text}</p>
+            </Collapse.Panel>
+          </Collapse>
+        </Form>
+
       </Drawer>
     </>
   )
